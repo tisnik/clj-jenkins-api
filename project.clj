289ccd50ae4d9d609b1d3358272d32d72caf5c6e
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-11-24 20:59:05 +0100

    Added licence into project file project.clj

---
+++ project.clj
@@ -0,0 +1,21 @@
+;
+;  (C) Copyright 2015  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(defproject clj-jenkins-api "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main ^:skip-aot clj-jenkins-api.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})

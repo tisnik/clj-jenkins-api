f699e39395eeb5ca1b420d675385c2b2cf71413a
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-11-25 22:13:27 +0100

    Stub for new Clojure project

---
+++ test/clj_jenkins_api/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-jenkins-api.core-test
+  (:require [clojure.test :refer :all]
+            [clj-jenkins-api.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

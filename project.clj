4ea270e5617bceb088a74f397aea5ed57e3cdd5b
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-01-22 13:07:26 +0100

    New plugins: text2junit, codox and lein-cloverage

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;
-;  (C) Copyright 2015  Pavel Tisnovsky
+;  (C) Copyright 2015, 2016  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -10,12 +10,12 @@
 ;      Pavel Tisnovsky
 ;
 
-(defproject clj-jenkins-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :main ^:skip-aot clj-jenkins-api.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+(defproject org.clojars.tisnik/clj-jenkins-api "0.1.0-SNAPSHOT"
+    :description "FIXME: write description"
+    :url "http://example.com/FIXME"
+    :license {:name "Eclipse Public License"
+              :url "http://www.eclipse.org/legal/epl-v10.html"}
+    :dependencies [[org.clojure/clojure "1.6.0"]]
+    :plugins [[test2junit "1.1.0"]
+              [codox "0.8.11"]
+              [lein-cloverage "1.0.6"]])

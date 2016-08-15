71d47dcca120aa14e728c30a6120edd642c33f4e
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-08-15 10:40:54 +0200

    New functions: get-command and job-name->url

--- src/clj_jenkins_api/jenkins_api.clj
+++ src/clj_jenkins_api/jenkins_api.clj
@@ -1,5 +1,5 @@
 ;
-;  (C) Copyright 2015  Pavel Tisnovsky
+;  (C) Copyright 2015, 2016  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -10,5 +10,24 @@
 ;      Pavel Tisnovsky
 ;
 
-(ns clj-jenkins-api.jenkins-api)
+(ns clj-jenkins-api.jenkins-api
+    "Module with functions implementing Jenkins API.")
+
+(require '[clj-http.client   :as http-client])
+
+(defn get-command 
+    "The reimplementation of HTTP GET command that can use JVM keystore for
+     establishing secure connections."
+    [url]
+    (:body (http-client/get url {
+                    :keystore "keystore"
+                    :keystore-pass "changeit"
+                    :trust-store "keystore"
+                    :trust-store-pass "changeit"})))
+
+(defn job-name->url
+    "Transform job name (that can contain spaces) to the URL part."
+    [jenkins-url job-name]
+    (str jenkins-url "job/" (.replaceAll job-name " " "%20")))
+
 

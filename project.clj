644e799b3e4e785c36e1436b16cbe83aa4f04269
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-11-17 11:46:09 +0100

    Log4j library

--- project.clj
+++ project.clj
@@ -17,7 +17,13 @@
               :url "http://www.eclipse.org/legal/epl-v10.html"}
     :dependencies [[org.clojure/clojure "1.7.0"]
                    [org.clojure/data.json "0.2.5"]
-                   [clj-http "2.0.0"]]
+                   [clj-http "2.0.0"]
+                   [org.clojure/tools.logging "0.3.1"]
+                   [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
+                                                      javax.jms/jms
+                                                      com.sun.jmdk/jmxtools
+                                                      com.sun.jmx/jmxri]]
+                   [org.slf4j/slf4j-log4j12 "1.6.6"]]
     :plugins [[test2junit "1.1.0"]
               [codox "0.8.11"]
               [lein-cloverage "1.0.6"]]

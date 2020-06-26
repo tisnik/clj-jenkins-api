4cf7a3cae44891e5abb0e32a91df041e14dbd68e
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-26 08:50:29 +0200

    Reformatted project.clj by zprint

--- project.clj
+++ project.clj
@@ -11,22 +11,22 @@
 ;
 
 (defproject org.clojars.tisnik/clj-jenkins-api "0.10.0-SNAPSHOT"
-    :description "Set of functions to communicate with Jenkins via its API."
-    :url "https://github.com/tisnik/clj-jenkins-api"
-    :license {:name "Eclipse Public License"
-              :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.7.0"]
-                   [org.clojure/data.json "0.2.5"]
-                   [clj-http "2.0.0"]
-                   [org.clojure/tools.logging "0.3.1"]
-                   [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
-                                                      javax.jms/jms
-                                                      com.sun.jmdk/jmxtools
-                                                      com.sun.jmx/jmxri]]
-                   [org.slf4j/slf4j-log4j12 "1.6.6"]]
-    :plugins [[test2junit "1.1.0"]
-              [codox "0.8.11"]
-              [lein-cloverage "1.0.6"]]
-    :main ^:skip-aot clj-jenkins-api.jenkins-api
-    :target-path "target/%s"
-    :profiles {:uberjar {:aot :all}})
+  :description "Set of functions to communicate with Jenkins via its API."
+  :url "https://github.com/tisnik/clj-jenkins-api"
+  :license {:name "Eclipse Public License",
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.json "0.2.5"]
+                 [clj-http "2.0.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jmdk/jmxtools
+                                                    com.sun.jmx/jmxri]]
+                 [org.slf4j/slf4j-log4j12 "1.6.6"]]
+  :plugins [[test2junit "1.1.0"]
+            [codox "0.8.11"]
+            [lein-cloverage "1.0.6"]]
+  :main ^:skip-aot clj-jenkins-api.jenkins-api
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
